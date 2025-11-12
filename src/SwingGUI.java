cf2c5e11dbc5309d4aa2f4f1e12326e3edbfd0a8
astranono <90936000+astranono@users.noreply.github.com>
2025-11-12 09:56:51 -0500

    started UI, and logic.

--- src/SwingGUI.java
+++ src/SwingGUI.java
@@ -6,4 +6,11 @@ public class SwingGUI {
     private JButton startButton;
     private JButton cancelButton;
     private JTextArea textArea1;
+    private JButton wscrapeButton;
+    private JButton HTMLButton;
+    private JButton emailsButton;
+    private JTextPane textPane1;
+    private JButton wowButton;
+    private JButton copyButton1;
+    private JButton noIdeaButton;
 }
