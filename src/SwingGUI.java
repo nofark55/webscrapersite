da51ba5764d485508b89e41a35cd34ceeb8cfa30
astranono <90936000+astranono@users.noreply.github.com>
2025-11-11 23:58:18 -0500

    started UI, and logic.

---
+++ src/SwingGUI.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class SwingGUI {
+    private JPanel panel1;
+    private JButton submitButton;
+    private JButton startButton;
+    private JButton cancelButton;
+    private JTextArea textArea1;
+}
